9b0adfb6353bf0c1cca2d8bda876ec39587c55aa
m-tech-cod <malomonaraffath@gmail.com>
2026-07-05 12:09:46 +0100

    configuration @astrojs/sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  integrations: [tailwind()],
+  site: 'https://alaydetech.com',
+  integrations: [
+    tailwind(), 
+    sitemap()
+  ],
 });
